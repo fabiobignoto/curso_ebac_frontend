a70422657b0fdfd6c5f4d7dbef0885e1aa5b912e
FabioBignoto <fabiopalacio14@gmail.com>
2023-06-28 10:23:19 -0300

    Commit do arquivo .ts com a atividade do módulo

---
+++ atividade.ts
@@ -0,0 +1,7 @@
+function multiplica(primeiroNumero: number, segundoNumero: number):number {
+    return primeiroNumero * segundoNumero;
+}
+
+function saudacao (nome:string) : string {
+    return `Olá ${nome}!`
+}
